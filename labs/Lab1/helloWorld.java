5d28600a3777086d7bf775f9990d8048b5add0eb
Dinmukhamed Madiyar <madiyardinmukhamed@gmail.com>
2025-12-31 23:46:41 +0500

    added files via upload

---
+++ labs/Lab1/helloWorld.java
@@ -0,0 +1,12 @@
+package Lab1;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        };
+    }
+}
+
